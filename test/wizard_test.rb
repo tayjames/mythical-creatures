85a0302466a7608fef25167b1f6b40f5b8794d9d
Tay <tay.james.d@gmail.com>
2019-11-30 13:21:08 -0700

    Completes wizard

---
+++ test/wizard_test.rb
@@ -0,0 +1,64 @@
+gem 'minitest', '~> 5.2'
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/wizard'
+
+class WizardTest < Minitest::Test
+  def test_has_name
+    wizard = Wizard.new("Eric")
+    assert_equal "Eric", wizard.name
+  end
+
+  def test_can_have_different_name
+
+    wizard = Wizard.new("Alex")
+    assert_equal "Alex", wizard.name
+  end
+
+  def test_is_bearded_by_default
+
+    wizard = Wizard.new("Ben")
+    assert wizard.bearded?
+  end
+
+  def test_is_not_always_bearded
+
+    wizard = Wizard.new("Valerie", bearded: false)
+    refute wizard.bearded?
+  end
+
+  def test_has_root_powers
+
+    wizard = Wizard.new("Sarah", bearded: false)
+    assert_equal "sudo chown ~/bin", wizard.incantation("chown ~/bin")
+  end
+
+  def test_has_lots_of_root_powers
+
+    wizard = Wizard.new("Rob", bearded: false)
+    assert_equal "sudo rm -rf /home/mirandax", wizard.incantation("rm -rf /home/mirandax")
+  end
+
+  def test_starts_rested
+
+    wizard = Wizard.new("Rob", bearded: false)
+    assert_equal wizard.rested?, true
+  end
+
+  def test_can_cast_spells
+    wizard = Wizard.new("Rob", bearded: false)
+    assert_equal wizard.cast, "MAGIC MISSILE!"
+  end
+
+  def test_gets_tired_after_casting_three_spells
+    wizard = Wizard.new("Rob", bearded: false)
+    wizard.cast
+    wizard.cast
+    assert_equal wizard.rested?, true
+
+    wizard.cast
+    refute wizard.rested?
+  end
+
+end
